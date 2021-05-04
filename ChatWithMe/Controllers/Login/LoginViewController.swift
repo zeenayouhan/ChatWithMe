dca269920abd4bd43f987627b39ce4ead1a78c29
zeenayouhan <zinayouhan33@gmail.com>
2021-05-04 20:39:00 +0530

    Register

--- ChatWithMe/Controllers/Login/LoginViewController.swift
+++ ChatWithMe/Controllers/Login/LoginViewController.swift
@@ -52,22 +52,37 @@ class LoginViewController: UIViewController {
         field.isSecureTextEntry = true
         return field
     }()
-
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .green
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        return button
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log In"
         view.backgroundColor = .white
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style:  .done, target: self, action: #selector(didTapRegister))
-        
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        emailField.delegate = self
+        passwordField.delegate = self
         view.addSubview(scrollView)
         scrollView.addSubview(imageView)
         scrollView.addSubview(emailField)
         scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
         
         
-
-}
+        
+    }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         scrollView.frame = view.bounds
@@ -78,6 +93,28 @@ class LoginViewController: UIViewController {
         )
         passwordField.frame = CGRect(x: 30, y: emailField.bottom+10, width: scrollView.width-60,height: 52
         )
+        loginButton.frame = CGRect(x: 30, y: passwordField.bottom+10, width: scrollView.width-60,height: 52
+        )
+    }
+    
+    @objc private func loginButtonTapped(){
+        emailField.resignFirstResponder()
+        passwordField.resignFirstResponder()
+         
+        guard let email = emailField.text, let password =  passwordField.text,
+              !email.isEmpty, !password.isEmpty, password.count>=6 else{
+            alertUserLoginError()
+            return
+        }
+        
+        //firebase login
+    }
+    
+    func alertUserLoginError(){
+        let alert = UIAlertController(title: "Woops", message: "Please enter all information to log in.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title:"Dismiss", style: .cancel, handler: nil))
+        present(alert, animated: true)
+        
     }
     
     @objc private func didTapRegister(){
@@ -86,4 +123,17 @@ class LoginViewController: UIViewController {
         
         navigationController?.pushViewController(vc, animated: true)
     }
+}
+
+extension LoginViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailField{
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField{
+            loginButtonTapped()
+        }
+        return true
+    }
+    
 }
